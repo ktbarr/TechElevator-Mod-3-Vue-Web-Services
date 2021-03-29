2942c63e3e9b498e0ece7175e5059460805a29de
Katie Barr <76793948+ktbarr@users.noreply.github.com>
2021-03-29 15:03:33 -0400

    Add files via upload

---
+++ student-tutorial-Post/src/services/DocsService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: "http://localhost:3000"
+});
+
+export default {
+
+  list() {
+    return http.get('/docs');
+  },
+
+  get(id) {
+    return http.get(`/docs/${id}`)
+  },
+
+  create(doc) {
+    return http.post(`/docs/`, doc);
+  }
+
+}
